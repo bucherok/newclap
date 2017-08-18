0ccde4b7029cfc855c93beb54a1f5f74862f3a41
bucherok <bucherok@ya.ru>
2017-08-18 14:48:50 +0300

    Initial

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <div class="header_content">
+            <a href="">ВОЙТИ ИЛИ ЗАРЕГИСТРИРОВ1111АТТЬСЯ</a>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data: function () {
+      return {
+        user: ''
+      }
+    }
+  }
+</script>
+<style lang="scss">
+    .header{
+        background-color: #292a2a;
+        &_content{
+            width: 970px;
+            margin:0 auto;
+        }
+    }
+
+
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <span>Это плеер</span>
+        <router-link to="/">Home</router-link>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'header',
+    data: function () {
+      return {
+        user: ''
+      }
+    }
+  }
+</script>
+<style scoped="player">
+    span {
+        color: #000;
+    }
+</style>
